dd1d1437ae85db31149afbf78ea841bd8dd9533c
Bishnu Kumar Das <bishnu.das@ycotek.com>
2026-02-24 13:00:21 +0545

    cleanup (#9)

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -62,10 +62,8 @@ export const parseSchema = (dbmlString: string): SchemaData => {
       }
     })
 
-    console.log(tables)
     return { tables, refs, fetchedCols, errors }
   } catch (e) {
-    console.error(e)
     return { tables, refs, fetchedCols, errors }
   }
 }
